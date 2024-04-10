ffff252591273b95949d184ca6b47c1d1fb8c577
Dondou <106757087+dondou21@users.noreply.github.com>
2024-04-10 20:23:20 +0200

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
-const dict = require('./101-data').dict;
-console.log(Object.entries(dict).reduce(function (i, idx) {
-  i[idx[1]] = (i[idx[1]] || []).concat(idx[0]);
-  return i;
-}, {}));
+const dict = require('./101-data.js').dict;
+const newDict = {};
+for (const key in dict) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [];
+  }
+  newDict[dict[key]].push(key);
+}
+console.log(newDict);
