c7fa9d057c57eee57e403fbe547bb930304e6e10
Dondou <106757087+dondou21@users.noreply.github.com>
2024-04-30 07:57:48 +0200

    Update 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,31 +1,14 @@
 #!/usr/bin/python3
+"""A script that:
+- takes your GitHub credentials (username and password)
+- uses the GitHub API to display your id
 """
-module to print ones github id
-One will need GH PAT for this
-"""
-
-from requests import get
-from sys import argv
+import sys
+import requests
 from requests.auth import HTTPBasicAuth
 
 
-def get_github_id(username: str, tokken: str) -> str:
-    """
-    get the user id of a github user
-    Args:
-        username (str): github handle
-        tokken (str): the personal access tokken
-    """
-    headers = {
-        "Accept": "application/vnd.github+json",
-        "X-GitHub-Api-Version": "2022-11-28",
-        "Authorization": "Bearer {}".format(tokken),
-        "User-Agent": "I am the Cavalry"
-    }
-    auth = HTTPBasicAuth(username, tokken)
-    response = get("https://api.github.com/user", headers=headers, auth=auth)
-    return response.json().get('id')
-
-
 if __name__ == "__main__":
-    print(get_github_id(argv[1], argv[2]))
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
