fccdb74a37ac1fbd4522704677dfbcca0fc4bd37
Dondou <106757087+dondou21@users.noreply.github.com>
2024-04-30 08:10:20 +0200

    Update 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-"""Send a request to the url and print the response"""
-
-from urllib import request, error
-from sys import argv
-
-
-def request_header_property(url: str) -> str:
-    """
-    Send a request to the URL specified and
-    get the response and handle exceptions
-    Args:
-        url (str): The URL to query
-    """
-    try:
-        with request.urlopen(url) as response:
-            return response.read().decode('utf-8')
-    except error.HTTPError as e:
-        return "Error code: {}".format(e.code)
+"""A script that:
+- takes in a URL,
+- sends a request to the URL
+- displays the body of the response (decoded in utf-8).
+"""
 
 
 if __name__ == "__main__":
-    print(request_header_property(argv[1]))
+    import sys
+    from urllib import request, error
+
+    try:
+        with request.urlopen(sys.argv[1]) as res:
+            print(res.read().decode('UTF-8'))
+    except error.HTTPError as er:
+        print('Error code:', er.code)
