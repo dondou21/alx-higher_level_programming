f088cb74e01e2ad6d414560a223f294b1c6e551c
Dondou <106757087+dondou21@users.noreply.github.com>
2024-04-30 08:03:01 +0200

    Update 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
-"""Send a request to the url and print the response"""
-
-from requests import get
-from sys import argv
-
-
-def request_header_property(url: str) -> str:
-    """
-    Send a request to the URL specified and
-    get the response and handle exceptions
-    Args:
-        url (str): The URL to query
-    """
-    response = get(url)
-    if int(response.status_code) >= 400:
-        return ("Error code: {}".format(response.status_code))
-
-    return response.text
+"""A script that
+- takes in a URL
+- sends a request to the URL
+- displays the body of the response.
+"""
+import sys
+import requests
 
 
 if __name__ == "__main__":
-    print(request_header_property(argv[1]))
+    url = sys.argv[1]
+
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
