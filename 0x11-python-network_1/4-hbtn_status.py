3d3902880358df920f80d1eab7015c50d09f7928
Dondou <106757087+dondou21@users.noreply.github.com>
2024-04-30 08:07:43 +0200

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,23 +1,10 @@
 #!/usr/bin/python3
-
-"""
-get https://alx-intranet.hbtn.io/status using
-requests and print the response to stdout
-"""
-
-from requests import get
-
-
-def get_alx_intranet(url='https://alx-intranet.hbtn.io/status'):
-    """
-    Send a GET request to the url
-    and print the reponse
-    """
-    res = get(url)
-    print("Body response:")
-    print("\t- type: {}".format(str(res).__class__))
-    print("\t- content: {}".format(res.text))
+"""fetches https://intranet.hbtn.io/status."""
+import requests
 
 
 if __name__ == "__main__":
-    get_alx_intranet()
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
