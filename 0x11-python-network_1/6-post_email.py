9f7102bbc0cb5e9767b4ee18623d66625e77ecf9
Dondou <106757087+dondou21@users.noreply.github.com>
2024-04-30 08:03:39 +0200

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,25 +1,15 @@
 #!/usr/bin/python3
-"""send an email to the url and print the response"""
-
-from requests import post
-from sys import argv
-
-
-def send_email_to_url(url: str, email: str) -> str:
-    """
-    Send a request to the URL specified and
-    get the response
-    Args:
-        url (str): The URL to send to
-        email (str): the email addr
-    """
-    data = {}
-    data['email'] = email
-    try:
-        return (post(url, data=data).text)
-    except Exception as e:
-        return e
-
+"""A script that:
+- takes in a URL,
+- sends a request to the URL and displays the value
+- of the X-Request-Id variable found in the header ofthe response.
+"""
+import sys
+import urllib.request
 
 if __name__ == "__main__":
-    print(send_email_to_url(argv[1], argv[2]))
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
